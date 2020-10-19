392dd119d35e4e6278241a988974680e4c9d2e20
arpit-mishra2019 <arpit.mishramivi@gmail.com>
2020-10-19 12:15:23 +0000

    commited

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from flask import Flask 
-app = Flask(__name__) 
+app = Flask(__name__, static_url_path='/static') 
 @app.route("/") 
 def hello():
-      return "Hello, Welcom!" 
+      return app.send_static_file('index.html')
 if __name__ == "__main__":
-      app.run()
+      app.run(host='0.0.0.0')
